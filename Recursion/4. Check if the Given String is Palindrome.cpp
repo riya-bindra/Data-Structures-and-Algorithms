c199c91b7ecacb662bec481d7b4f068b7934d975
Riya <40891460+riya-bindra@users.noreply.github.com>
2021-08-05 12:08:20 +0530

    Add files via upload

---
+++ Recursion/4. Check if the Given String is Palindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isPalindrome(string &str, int start, int end)
+{
+	if(start>=end)
+		return true;
+	return (str[start]==str[end] && isPalindrome(str,start+1,end-1));
+}
+
+int main()
+{
+	string str="cabbac";
+	cout<<isPalindrome(str,0,5)<<endl;
+	string str2="abcba";	
+	cout<<isPalindrome(str2,0,4)<<endl;
+	string str3="aaaaaa";
+	cout<<isPalindrome(str3,0,5)<<endl;	
+	string str4="madam";
+	cout<<isPalindrome(str4,0,4)<<endl;
+	string str5="Riya";
+	cout<<isPalindrome(str5,0,3)<<endl;	
+	return 0;
+}
+
+//Time Complexity: O(n)
+//Auxiliary Space: O(n)
