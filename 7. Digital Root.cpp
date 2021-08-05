8029b0fd097922758644c1f6b0e556472acb0f13
Riya <40891460+riya-bindra@users.noreply.github.com>
2021-08-05 12:52:22 +0530

    Create 7. Digital Root.cpp

---
+++ 7. Digital Root.cpp
@@ -0,0 +1,23 @@
+
+/*You are given a number n. You need to find the digital root of n. DigitalRoot of a number is the recursive sum of its digits until we get a single digit number.
+Problem link: https://practice.geeksforgeeks.org/problems/digital-root/1/?track=DSASP-Recursion&batchId=154#
+*/
+
+class Solution{
+  public:
+    //Complete this function
+    int digitalRoot(int n)
+    {
+        //Your code here
+    	if(n>=0 && n<=9)
+		return n;
+	int s=0;
+	while(n>0)
+	{
+		s=s+(n%10);
+		n=n/10;
+	}
+	return digitalRoot(s);
+    }
+};
+
